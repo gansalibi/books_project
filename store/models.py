bb27f15849a54dea5184945eb4a478b4109cdb69
gansalibi <gansalibi@gmail.com>
2023-05-21 13:48:38 +0300

    Setup filtering, ordering and searching in BookViewSet Registration model Book in Admin with Decorator

--- store/models.py
+++ store/models.py
@@ -4,5 +4,9 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(decimal_places=2, max_digits=7)
+    author = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'ID {self.id} {self.name}'
 
 
--- store/views.py
+++ store/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Book
@@ -7,4 +9,8 @@ from store.serializers import BooksSerializer
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
-
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filterset_fields = ['price']
+    ordering_fields = ['price', 'name', 'author']
+    ordering = ['-price']
+    search_fields = ['name', 'price', 'author']
